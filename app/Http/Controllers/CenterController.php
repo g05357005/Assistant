09d3377e9a96bd620d726194ad8e2e9d38b90cc3
fred07 <fivesec10@gmail.com>
2016-12-24 00:51:43 +0800

    Create echo controller

---
+++ app/Http/Controllers/CenterController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use LINE\LINEBot\Constant\HTTPHeader;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot\Event\MessageEvent;
+use LINE\LINEBot;
+
+class CenterController extends Controller
+{
+    const CHANNEL_ACCESS_TOKEN = '5/e6JfcucgEbWqGb7+pfaAErw3BbhzM9VGA3fMOdP0F9m20q2DwAe7jSazDDGD9pL/5Z2lAAhluaD+zC6mxOjzbJbX/amlNDqaZuuiaAWn9BE9KbpE4nCIWazHiCv7VVqsZBcJU0Lwl6HmEhRSjlIgdB04t89/1O/w1cDnyilFU=';
+    const CHANNEL_SECRET = '8e0ee0e6b36483a1dd4a2a410147962c';
+
+    private $httpClient;
+    private $bot;
+
+    public function __construct()
+    {
+        // Initialize
+        $this->httpClient = new CurlHTTPClient(self::CHANNEL_ACCESS_TOKEN);
+        $this->bot        = new LINEBot($this->httpClient, ['channelSecret' => self::CHANNEL_SECRET]);
+    }
+
+    public function echo(Request $request)
+    {
+        // Check header from LINE
+        $signature = $request->header(HTTPHeader::LINE_SIGNATURE);
+        if (empty($signature)) {
+            // return $request->withStatus(400, 'Bad Request');
+        }
+
+        // Get event
+        $events = $this->bot->parseEventRequest($request->all(), $signature[0]);
+
+        foreach ($event as $event) {
+            if ($event instanceof MessageEvent) {
+                $text = $event->getText();
+
+                $response = $bot->replyText($event->getReplyToken(), $text);
+            }
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:api');
+
+Route::post('getEcho', 'CenterController@echo');
+
+Route::get('getEcho', 'CenterController@echo');
