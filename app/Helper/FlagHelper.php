cd53624dd9f4ef8a37485eb5a6e3a8792ad0432b
fred07 <fivesec10@gmail.com>
2017-01-06 01:55:56 +0800

    Add FlagHelper

---
+++ app/Helper/FlagHelper.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Helper;
+
+class FlagHelper
+{
+    private $base;
+
+    /**
+     * @param $base int base value for flag
+     * @return void
+     */
+    public function __construct($base)
+    {
+        $this->base = $base;
+    }
+
+    public function getValue()
+    {
+        return $this->base;
+    }
+
+    /**
+     * Get flag in binary string
+     * @return string
+     */
+    public function getBinaryStr()
+    {
+        return decbin($this->base);
+    }
+
+    public function isOn($value)
+    {
+        return $this->base & $value;
+    }
+
+    public function isOff($value)
+    {
+        return !$this->isOn($value);
+    }
+
+    public function setOn($value)
+    {
+        $this->base = $this->base | $value;
+    }
+
+    public function setOff($value)
+    {
+        $this->base = $this->base & (~$value);
+    }
+}
